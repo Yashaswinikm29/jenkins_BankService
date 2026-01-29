665128dccbe70eca3279aa8320be64b3501b424d
Yashaswinikm29 <yashaswinikm.2023@gmail.com>
2026-01-29 17:44:37 +0530

    Update deposit amount in BankService

--- BankService.java
+++ BankService.java
@@ -27,7 +27,7 @@ class BankAccount {
 public class BankService {
     public static void main(String[] args) {
         BankAccount acc = new BankAccount(1000);
-        acc.deposit(500);
+        acc.deposit(5000);
         acc.withdraw(100);
         System.out.println("Final Balance: " + acc.getBalance());
     }
